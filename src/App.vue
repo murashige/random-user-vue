1b227976a8a9279eb2bb8599cb09cea1f098f74f
村重司 <tsukasa@murashigetsukasanoMacBook-Pro-2.local>
2021-03-14 13:06:52 +0900

    Minor fixes

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,45 @@
 <template>
   <div class="wrapper">
     <SelectGender @change-gender="changeGender($event)" class="select-gender" />
-    <pulse-loader v-if="isLoading" class="loader-block"></pulse-loader>
 
-    <div v-else class="columns is-multiline">
-      <User v-for="user in users" :key="user.email" :user="user" />
+    <div class="users-block">
+      <pulse-loader v-if="isLoading" class="loader-block"></pulse-loader>
+      <div v-else class="columns is-multiline">
+        <User v-for="user in users" :key="user.email" :user="user" />
+      </div>
     </div>
+
+    <paginate
+      v-model="requestParams.page"
+      :page-count="totalPages"
+      :click-handler="changePage"
+      :prev-text="'Prev'"
+      :next-text="'Next'"
+      :container-class="'pagination is-centered'"
+      :page-link-class="'pagination-link'"
+      :prev-link-class="'pagination-previous'"
+      :next-link-class="'pagination-next'"
+      :break-view-link-class="'pagination-ellipsis'"
+      :active-class="'is-current'"
+      :no-li-surround="true"
+    >
+    </paginate>
   </div>
 </template>
 
 <script>
 import PulseLoader from 'vue-spinner/src/PulseLoader'
+import Paginate from 'vuejs-paginate'
 
 import { fetchUsersRequest } from './api/fetchUsers'
 import SelectGender from './components/SelectGender'
 import User from './components/User'
 
-const TOTAL_PAGES = 30
-
 export default {
   name: 'App',
   components: {
     PulseLoader,
+    Paginate,
     SelectGender,
     User,
   },
@@ -36,6 +54,7 @@ export default {
         inc: 'gender,name,location,email,phone,picture',
         gender: 'both',
       },
+      totalPages: 30,
     }
   },
   computed: {
@@ -60,6 +79,11 @@ export default {
       this.$set(this.requestParams, 'page', 1)
       this.getUsers(this.requestParams)
     },
+    changePage(page) {
+      this.$set(this.requestParams, 'page', page)
+      this.getUsers(this.requestParams)
+      window.scrollTo(0, 0)
+    },
   },
   created: function() {
     this.getUsers(this.requestParams)
@@ -74,10 +98,25 @@ export default {
 .select-gender {
   margin-bottom: 30px;
 }
+.users-block {
+  position: relative;
+  min-height: 500px;
+}
 .loader-block {
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+}
+.pagination {
+  max-width: 400px;
+  margin: 30px auto;
+}
+// 子コンポーネントにもstyleが効くようにする
+::v-deep .pagination-ellipsis {
+  border: none;
+}
+::v-deep .disabled {
+  opacity: 0.3;
 }
 </style>
